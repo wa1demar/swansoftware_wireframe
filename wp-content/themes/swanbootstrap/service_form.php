df9e2e6b7992dd84a8670b82d3619463bb2f309d
viktordanch <viktor.danch@gmail.com>
2013-10-17 18:02:20 +0300

    start page

---
+++ wp-content/themes/swanbootstrap/service_form.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *
+ * Template Name: Service Form Template
+ *
+ * Created by JetBrains PhpStorm.
+ * @author: Viktor
+ * @date: 10/17/13
+ *
+ */
+?>
+<?php get_header() ?>
+<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/css/service_form/main.css"/>
+<div id='sliderFirst'>
+    <?php $bg_image = "../'assets/pics/technology.png" ?>
+    <?php require "_social.php" ?>
+
+    <div class='row darck-background'>
+        <div class="container text-center">
+<!--            <div class="row">-->
+<!--                <div class='col-xs-4'>-->
+<!--                    <img src=" ">-->
+<!--                </div>-->
+<!--                <div class='col-xs-8'>-->
+<!--                    <div class='text'>-->
+<!--                        Complete the inquiry form below-->
+<!--                        <br />-->
+<!--                        and we will get back to you with our Proposal.-->
+<!--                        <br />-->
+<!--                        <b>Or call us: 317-691-3806</b>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+        </div>
+    </div>
+</div>
+
+<?php get_footer() ?>
+
+
