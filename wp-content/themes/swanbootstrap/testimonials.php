e54e41194d7b545f501ac89e1ac2e768e12ac661
viktordanch <viktor.danch@gmail.com>
2013-10-17 14:07:24 +0300

    start page

---
+++ wp-content/themes/swanbootstrap/testimonials.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * Template Name: Testimonials
+ *
+ * Created by JetBrains PhpStorm.
+ * User: Viktor
+ * Date: 10/17/13
+ * Time: 1:32 PM
+ */
+?>
+<?php get_header() ?>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/css/testimonials/main.css"/>
+    <div id='sliderFirst'>
+        <?php $bg_image = "../'assets/img/technology.png" ?>
+        <?php require "_social.php" ?>
+        <div class="container">
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+<?php get_footer() ?>
