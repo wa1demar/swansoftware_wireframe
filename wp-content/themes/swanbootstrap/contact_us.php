f32a0a3b5e2a4c2b8c2c0def68b83ce8ae923223
waldemar <zehelloworld@gmail.com>
2013-10-18 14:12:35 +0300

    create contact us wp template

---
+++ wp-content/themes/swanbootstrap/contact_us.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Template Name: Contact Us Template
+ *
+ * Created by PhpStorm.
+ * @author: waldemar
+ * @date: 18.10.13
+ *
+ * @note: менять ничего не рекомендую, хрупкий баланс ошибок,
+ * компенсирующих друг друга, заставляет программу работать
+ */
+?>
+
+<?php get_header()?>
+<?php while ( have_posts() ) : the_post(); ?>
+    <?php $bg = get_field('background'); ?>
+    <div id="sliderFirst" style="background:  url('<?= $bg ?>') center top repeat;">
+        <div class="info-section">
+            <?php addSocials($bg)?>
+
+            <div class="row dark-background">
+
+                <div class="col-lg-12 text-center background">
+
+                    <div class="col-lg-12  contact-text-phone ">
+                        <div class='container'>
+                            <h1>Contact us</h1>
+
+                            <div class="col-sm-5 text-section">
+                                <div class="text-left" >
+                                    <b>Corporate Headquarters </b>
+
+                                    <div class='text'>
+                                        698 Pro-med Ln, Ste 200 Carmel IN 46032
+                                    </div>
+                                    <b>Drect Lines </b>
+
+                                    <div class='text'>
+                                        317-691-3806
+                                        <br/>
+                                        317-362-9017
+                                    </div>
+                                    <b>Email addresses </b>
+
+                                    <div class='text'>
+                                        info@swansoftwaresolutions.com
+                                        <br/>
+                                        sales@swansoftwaresolutions.com
+                                        <br/>
+                                        executive@swansoftwaresolutions.com
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-sm-7 form-section">
+                                <form>
+                                    <div class="row">
+                                        <div class="col-xs-3 label">
+                                            Name
+
+
+                                            <img src="<?php bloginfo('template_directory') ?>/assets/img/name.png">
+                                        </div>
+                                        <div class="col-xs-8"><input type="text" placeholder="  John Doe...">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xs-3 label">
+                                            Email
+
+                                            <img src="<?php bloginfo('template_directory') ?>/assets/img/email.png">
+                                        </div>
+                                        <div class="col-xs-8">
+                                            <input type="text"
+                                                   placeholder="  johndoe@jahoo.com">
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-xs-3 label">
+                                            Comment
+
+
+                                            <img src="<?php bloginfo('template_directory') ?>/assets/img/comment.png">
+                                        </div>
+                                        <div class="col-xs-8"><textarea
+                                                placeholder="  Type your messahe here..."></textarea>
+                                        </div>
+                                    </div>
+                                    <a href="#" class="blue_btn small">Send</a>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="sliderSecond">
+        <div class='section'>
+        </div>
+    </div>
+<?php endwhile; ?>
+<?php get_footer()?>
--- wp-content/themes/swanbootstrap/header.php
+++ wp-content/themes/swanbootstrap/header.php
@@ -1,34 +1,38 @@
 <?php
 /**
-*
-* Created by PhpStorm.
-* @author: waldemar
-* @date: 16.10.13
-*
-* @note: менять ничего не рекомендую, хрупкий баланс ошибок,
-* компенсирующих друг друга, заставляет программу работать
-*/?>
+ *
+ * Created by PhpStorm.
+ * @author: waldemar
+ * @date: 16.10.13
+ *
+ * @note: менять ничего не рекомендую, хрупкий баланс ошибок,
+ * компенсирующих друг друга, заставляет программу работать
+ */
+?>
 
 <!DOCTYPE html>
-<html <?php language_attributes()?>>
+<html <?php language_attributes() ?>>
 <head>
-    <title><?php wp_title('|', true, 'right')?></title>
+    <title><?php wp_title('|', true, 'right') ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta charset="<?php bloginfo('charset')?>"/>
-    <link href="<?php bloginfo('template_directory')?>/assets/css/libs/bootstrap.min.css" rel="stylesheet"/>
-    <link rel="stylesheet" href="<?php bloginfo('template_directory')?>/assets/css/globals/global.css"/>
+    <meta charset="<?php bloginfo('charset') ?>"/>
+    <link href="<?php bloginfo('template_directory') ?>/assets/css/libs/bootstrap.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/css/globals/global.css"/>
 
     <?php if (is_page_template('home.php')): ?>
-        <link rel="stylesheet" href="<?php bloginfo('template_directory')?>/assets/css/home/main.css"/>
+        <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/css/home/main.css"/>
     <?php elseif (is_page_template('services.php')): ?>
-        <link rel="stylesheet" href="<?php bloginfo('template_directory')?>/assets/css/services/main.css"/>
-    <?php elseif (is_home()): ?>
-        <link rel="stylesheet" href="<?php bloginfo('template_directory')?>/assets/css/home/main.css"/>
+        <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/css/services/main.css"/>
+    <?php elseif (is_page_template('contact_us.php')): ?>
+        <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/css/contact_us/main.css"/>
+    <?php
+    elseif (is_home()): ?>
+        <link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/css/home/main.css"/>
     <?php endif ?>
 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
-    <?php wp_head()?>
+    <?php wp_head() ?>
 </head>
 <body>
 
@@ -38,7 +42,7 @@
             <div class="col-lg-6 col-md-6 col-sm-7">
                 <div class="row">
                     <div class="col-lg-4 col-md-4 col-sm-4">
-                        <img src="<?php bloginfo('template_directory')?>/assets/img/swan_150x100.png">
+                        <img src="<?php bloginfo('template_directory') ?>/assets/img/swan_150x100.png">
                     </div>
                     <div class="col-lg-8 col-md-8 col-sm-8">
                         <hgroup>
@@ -64,7 +68,7 @@
                 </div>
                 <div class="row ">
                     <div class="col-lg-12 col-md-12">
-                        <?php wp_nav_menu(getArgsForMainMenu())?>
+                        <?php wp_nav_menu(getArgsForMainMenu()) ?>
                     </div>
                 </div>
 
