0a20d158e3ae53d845c3c04bb29cd4c2c3b7241e
waldemar <zehelloworld@gmail.com>
2013-10-25 20:52:40 +0300

    Create company page

--- wp-content/themes/swanbootstrap/company.php
+++ wp-content/themes/swanbootstrap/company.php
@@ -12,11 +12,10 @@
 ?>
 
 <?php get_header() ?>
+<?php while (have_posts()) : the_post(); ?>
 <?php $bg = get_field('background'); ?>
-<link rel="stylesheet" href="<?php bloginfo('template_directory') ?>/assets/css/company/main.css"/>
-<div id='sliderFirst'>
-    <?php $bg_image = "../'assets/pics/technology.png" ?>
-    <?php require "_social.php" ?>
+<div id='sliderFirst' style="background: url('<?= $bg ?>') center top repeat-x;">
+    <?php addSocials($bg) ?>
 
     <div class='row darck-background'>
         <div class="container text-center">
@@ -27,16 +26,7 @@
                     <h1>Allow us to introduce ourselves</h1>
 
                     <div class='text text-left'>
-                        Swan Software Solutions LLS is a leading provider of Custom Application Development, Mobile
-                        Application Development, Staff Augmentation, Web & Graphic Design, Internet Marketing and
-                        Superior
-                        Consulting Services.
-                        <br/>
-                        <br/>
-                        We are an energetic, growing company with exceptional knowledge and expertise in technological
-                        development. We are ready for any challenges
-                        that we are presented and are excited about upcoming opportunities.
-
+                       <?php the_content()?>
 
                     </div>
                 </div>
@@ -53,16 +43,15 @@
             <div class='services col-xs-5'>
                 <h1>Services</h1>
                 <ul class="secondSidebarMenu">
-                    <li data-target="#carousel-example-generic" data-slide-to="0" class="mobile active">
+                    <li class="mobile">
                         <a href="#">Mobile Development</a></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="1" class="staf"><a href="#">Staf
+                    <li class="staf"><a href="#">Staf
                             Augmentation</a></li>
-                    <li class="app" data-target="#carousel-example-generic" data-slide-to="2"><a href="#">Custom
+                    <li class="app"><a href="#">Custom
                             Application Development</a></li>
-                    <li class="design" data-target="#carousel-example-generic" data-slide-to="3"><a
+                    <li class="design" ><a
                             href="#">Design</a>
                     </li>
-                    <!--<li class="marketing"><a href="#">Internet Marketing</a></li>-->
                 </ul>
 
 
@@ -73,6 +62,7 @@
             <div class='customers col-xs-6 text-left'>
                 <div class='container'>
                     <h1>Customers</h1>
+                    <?=get_field('customers')?>
                 </div>
             </div>
         </div>
@@ -96,9 +86,9 @@
                 </div>
                 <div class='col-xs-6 text-left'>
                     <h1>Awards and Recognition</h1>
-                    <img src='<?php bloginfo('template_directory') ?>/assets/img/logo.png'>
-                    <img src='<?php bloginfo('template_directory') ?>/assets/img/logo.png'>
-                    <img src='<?php bloginfo('template_directory') ?>/assets/img/logo.png'>
+                    <img src='<?php bloginfo('template_directory') ?>/assets/img/green_business.png'>
+                    <img src='<?php bloginfo('template_directory') ?>/assets/img/children.png'>
+                    <img src='<?php bloginfo('template_directory') ?>/assets/img/talent.png'>
 
                 </div>
             </div>
@@ -112,7 +102,7 @@
 
                 <div class='row '>
                     <div class='col-xs-6'>
-                        <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
+                        <img src='<?php bloginfo('template_directory') ?>/assets/pics/al.png' class="img-circle">
 
                     </div>
 
@@ -137,13 +127,13 @@
                             </div>
                             <div class='col-xs-4 text-center'>
 
-                                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png'
+                                <img src='<?php bloginfo('template_directory') ?>/assets/pics/fel.png'
                                      class="img-circle">
 
                                 <span>Felix Shalit</span>
                             </div>
                             <div class='col-xs-4 text-center'>
-                                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png'
+                                <img src='<?php bloginfo('template_directory') ?>/assets/pics/anker.png'
                                      class="img-circle">
 
                                 <span>Terry Anker</span>
@@ -151,7 +141,7 @@
 
                             </div>
                             <div class='col-xs-4 text-center'>
-                                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png'
+                                <img src='<?php bloginfo('template_directory') ?>/assets/pics/alla.png'
                                      class="img-circle">
 
                                 <span>Alla Morozov</span>
@@ -167,6 +157,7 @@
         </div>
     </div>
 </div>
+<?php endwhile ?>
 <div id='sectionThird'>
     <div class='container'>
         <div class='row'>
@@ -174,30 +165,7 @@
                 <h1>Managers</h1>
 
                 <div class='row managers'>
-                    <div class='col-xs-2 text-center'>
-                        <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-                        <span>Sergey Zakharow</span>
-                    </div>
-                    <div class='col-xs-2 text-center'>
-                        <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-                        <span>Sergey Verevkin</span>
-                    </div>
-                    <div class='col-xs-2 text-center'>
-                        <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-                        <span>Rizak Vasyl</span>
-                    </div>
-                    <div class='col-xs-2 text-center'>
-                        <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-                        <span>Alexandr Povshuk</span>
-                    </div>
-                    <div class='col-xs-2 text-center'>
-                        <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-                        <span>Dmitriy Gvozdik</span>
-                    </div>
-                    <div class='col-xs-2 text-center'>
-                        <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-                        <span>Oleg Shvarts</span>
-                    </div>
+                    <?php addImages(7)?>
 
                 </div>
             </div>
@@ -227,96 +195,7 @@
             <div class='col-xs-12 text-center '>
                 <h1>Our Team of Superheroes</h1>
             </div>
-            <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-                <span>Brett Ridoux </span>
-                <br/>
-                Director of Business Development
-            </div>
-            <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-                <span>Anna Sabadosh</span>
-                <br/>
-                HR
-            </div>
-            <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>Lidiya Mykytyn</span>
-                <br/>
-                Reporting Manager
-            </div>
-            <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                tttttttttttttt
-            </div>
-            <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                Dt
-            </div>
-           <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                Dt
-            </div>
-           <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                Dt
-            </div>
-           <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                Dt
-            </div>
-           <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                Dt
-            </div>
-           <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                Dt
-            </div>
-           <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                Dt
-            </div>
-           <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                Dt
-            </div>
-           <div class='col-xs-2 text-center'>
-                <img src='<?php bloginfo('template_directory') ?>/assets/pics/ania.png' class="img-circle">
-
-                <span>tttttttttttt</span>
-                <br/>
-                Dt
-            </div>
-        </div>
+            <?php addImages(8)?>
     </div>
 </div>
 
