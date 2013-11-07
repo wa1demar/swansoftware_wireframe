5cd0579966fef95ec3b723c828301c38366232f8
viktordanch <viktor.danch@gmail.com>
2013-11-07 19:22:59 +0200

    update search form

--- wp-content/themes/swanbootstrap/_news.php
+++ wp-content/themes/swanbootstrap/_news.php
@@ -190,4 +190,91 @@ if (have_posts()) : ?>
             </div>
         </div>
     </div>
+    <?php else :?>
+    <?php
+    $args = array(
+        // Change these category SLUGS to suit your use.
+        'category_name' => 'News',
+        'posts_per_page' => 1
+    );
+
+    $last_post = new WP_Query($args);
+    ?>
+    <?php if ($last_post->have_posts()) : ?>
+        <?php while ($last_post->have_posts()) : $last_post->the_post(); ?>
+            <?php $last_post_ID = get_the_ID()?>
+
+            <?php $bg = get_bloginfo('template_directory') . '/assets/pics/technology.png' ?>
+            <div id='sliderFirst' style="background: url('<?= $bg ?>') center top;">
+                <?php addSocials($bg) ?>
+
+
+                <div class='row  darck-background'>
+                    <div class='container'>
+                        <div class='col-sm-8 img-background '
+                             style="background: url('<?php echo get_field('image') != "" ? get_field('image') : $default_img ?>') center no-repeat; background-size: 97% 100%"">
+                        <div class='col-xs-4 round-sign text-center'>
+                            <?= get_the_date('M') ?>
+                            <h1 class='roud'><?= get_the_date('d') ?></h1>
+                            <?= get_the_date('Y') ?>
+                        </div>
+                        <div class="shadow">
+                        </div>
+                        <div class='white-footer row'>
+
+
+                            <div class='col-xs-3'>
+
+                            </div>
+                            <div class='col-xs-9'>
+                                <b><?php the_title() ?></b><br/>
+                                <?php the_my_excerpt($post) ?>
+                                <br/>
+                                <a href="<?php the_permalink() ?>">View all +</a>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class='col-sm-4'>
+                        <h1 class='title'>Blog & News</h1>
+
+                        <form>
+                            <div class='inputs row'>
+                                <b>Join our Newsletter</b>
+                                <br/>
+
+                                <div class="col-xs-9 input1">
+                                    <input type='text' class="form-control " name='input1'
+                                           placeholder='Enter your Email address'>
+                                </div>
+                                <div class="col-xs-3">
+                                    <button type="button" class="form-control input_submit" name='submit1'>Go
+                                    </button>
+                                </div>
+                                <b>Search</b>
+
+                                <div class="col-xs-12">
+
+                                    <?php get_search_form()?>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+            </div>
+            <div id='not_found_section'>
+                <div class = 'rov'>
+                    <div class = 'coll-xs-12 text-center'>
+                        <h3>Not found any results for your request </h3>
+            </div>
+            </div>
+            </div>
+        <?php endwhile ?>
+    <?php endif ?>
+
 <?php endif ?>
